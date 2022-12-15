3a5f033c3f1c4a5a1d09f85767de31bc7cf1efae
sikander-afzaal <sikander.aaa@gmail.com>
2022-12-15 14:56:39 +0500

    asdasd

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,27 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+// postcss.config.js
+
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./node_modules/@brainhubeu/react-carousel/lib/style.css",
+      "./node_modules/@brainhubeu/react-carousel/lib/style.css.map",
+      "./node_modules/react-toastify/dist/*.css",
+      "./components/**/*.js",
+      "./pages/**/*.js",
+    ],
+    defaultExtractor: (content) => {
+      const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+      const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+      return broadMatches.concat(innerMatches);
+    },
   },
-}
+];
+module.exports = {
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
